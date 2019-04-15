7655ee6b8ee3e29d91be0a0867c97eaae9cc9dfb
Matt Carter <m@ttcarter.com>
2019-04-15 15:03:22 +1000

    BUGFIX: `o select` should also preserve the _collection meta key

--- functions/o.select.js
+++ functions/o.select.js
@@ -4,6 +4,7 @@ module.exports = o => {
 	o.cli
 		.description('Select a series of fields from an input collection')
 		.usage('<field...>')
+		.option('--no-collection', 'Also filter out the `_collection` meta key, which marks the source of the record when using a subsequent `o save` function')
 		.parse();
 
 	if (!o.cli.args.length) throw new Error('At least one field must be specified to select');
@@ -23,6 +24,7 @@ module.exports = o => {
 			_(doc)
 				.pick(includeFields)
 				.omit(excludeFields)
+				.thru(d => o.cli.collection && doc._collection ? _.set(d, '_collection', doc._collection) : d)
 				.value()
 		)
 	);
