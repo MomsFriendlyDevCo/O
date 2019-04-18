bf3d567d30c03fbcf68183131c551cdb7993e805
Matt Carter <m@ttcarter.com>
2019-04-18 14:59:38 +1000

    BUGFIX: `o sort` can now sort by outer object / omitted values like `o uniq`

--- functions/o.sort.js
+++ functions/o.sort.js
@@ -3,18 +3,23 @@ var siftShorthand = require('sift-shorthand');
 module.exports = o => {
 	o.cli
 		.description('Sort a collection of documents by given fields (this function BLOCKS)')
-		.usage('<fields...>')
+		.usage('[fields...]')
 		.option('-m, --memory', 'use in-memory caching instead of disk')
 		.note('Memory caching is extremely RAM intensive and large collections cause out-of-memory errors')
+		.note('If no fields are specified the top level element is used for comparison')
+		.note('Omit the field if the input is just an array of strings or numbers to compare those directly')
 		.parse();
 
 	var fields = siftShorthand.values(o.cli.args);
-	if (!_.keys(fields).length) throw new Error('At least one field must be specified to sort');
-	if (o.verbose) o.log(1, 'Sorting by', _.map(fields, (v, k) => (v ? '+' : '-') + k).join(', '));
+	if (o.verbose) o.log(1, 'Sorting by', fields.length ? _.map(fields, (v, k) => (v ? '+' : '-') + k).join(', ') : 'entire document');
 
 	if (o.cli.memory) { // Use memory method
 		o.on('collection', docs =>
-			o.output.collection(_.orderBy(docs, ..._.unzip(_.map(fields, (v, k) => [k, v ? 'asc' : 'desc']))))
+			o.output.collection(
+				o.cli.fields
+					? _.orderBy(docs, ..._.unzip(_.map(fields, (v, k) => [k, v ? 'asc' : 'desc'])))
+					: _.sortBy(docs)
+			)
 		);
 	} else { // Cache documents to disk storing their sort criteria, then reassemble and output
 		throw new Error('Only `o sort --memory` is supported at the moment');
