9644f26750247c757cf4d5111290ed6bdf8d928c
Matt Carter <m@ttcarter.com>
2019-04-18 15:27:20 +1000

    `o select` can now omit _id and all meta fields

--- functions/o.select.js
+++ functions/o.select.js
@@ -3,11 +3,15 @@ module.exports = o => {
 	o.cli
 		.description('Select a series of fields from an input collection')
 		.usage('<field...>')
+		.option('--no-meta', 'Also filter out the `_id` and `_collection` meta keys')
+		.option('--no-id', 'Also filter out the `_id` meta key')
 		.option('--no-collection', 'Also filter out the `_collection` meta key, which marks the source of the record when using a subsequent `o save` function')
 		.note('Fields can be prefixed with "!" to omit instead of include')
+		.note('`o select` and `o pluck` can both iterate down tree structures to obtain multiple results')
 		.parse();
 
 	if (!o.cli.args.length) throw new Error('At least one field must be specified to select');
+	if (o.cli.meta === false) o.cli.id = o.cli.collection = false;
 
 	var includeFields = [];
 	var excludeFields = [];
@@ -23,8 +27,9 @@ module.exports = o => {
 	o.on('doc', doc =>
 		o.output.doc(
 			_(doc)
-				.thru(v => includeFields.length ? _.pick(v, includeFields) : v)
+				.thru(v => includeFields.length ? includeFields.reduce((total, path) => _.set(total, path, o.utilities.getEndpoints(doc, path)), {}) : v)
 				.thru(v => excludeFields.length ? _.omit(v, excludeFields) : v)
+				.thru(d => o.cli.id && doc._id ? _.set(d, '_id', doc._collection) : _.omit(d, '_id'))
 				.thru(d => o.cli.collection && doc._collection ? _.set(d, '_collection', doc._collection) : _.omit(d, '_collection'))
 				.value()
 		)
